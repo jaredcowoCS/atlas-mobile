4152eb77a52e0ba4bec871e8e1a6f9be1636f781
Jared Cowo <jaredcowo@icloud.com>
2021-07-16 11:31:02 -0600

    Init xcode repo

---
+++ atlas-mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  atlas-mobile
+//
+//  Created by Jared Cowo on 7/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
